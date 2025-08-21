8e4e71a95a1e8ee11c4196b4598ff60852714d80
Søren Wittorff Sørensen <ery8my@rsyd.dk>
2025-08-21 13:36:52 +0200

    Load data

---
+++ Soren/Script S.R
@@ -0,0 +1,6 @@
+# Load packages and data
+
+library(tidyr)
+library(dplyr)
+vesuvius <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-13/vesuvius.csv')
+
