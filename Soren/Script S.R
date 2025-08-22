dfa8b579b655074ba7042b2a3fe9a1ad81c654a0
Søren Wittorff Sørensen <ery8my@rsyd.dk>
2025-08-22 09:57:34 +0200

    commit

--- Soren/Script S.R
+++ Soren/Script S.R
@@ -1,6 +1,39 @@
-# Load packages and data
-
+# Load packages and data ----
 library(tidyr)
 library(dplyr)
-vesuvius <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-13/vesuvius.csv')
+library(ggplot2)
+library(viridis)
 
+vesuvius <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-13/vesuvius.csv')
+str(vesuvius)
+
+# Missingness ----
+counts_NA <- colSums(is.na(vesuvius)) 
+perc_NA <- colMeans(is.na(vesuvius)) *100
+
+vesuvius_missingness <- data.frame(
+  var      = colnames(vesuvius),
+  counts_na = as.vector(counts_NA),
+  perc_na   = as.vector(perc_NA)
+) %>%
+  filter(perc_na > 0)
+print(vesuvius_missingness)
+
+# Plots ----
+# Global plot settings
+theme_set(theme_minimal())
+
+vesuvius %>%
+  filter(!is.na(duration_magnitude_md)) %>%
+  ggplot()+
+  geom_density(aes(x = time, 
+                   fill = cut(duration_magnitude_md, breaks = c(-Inf, 0, 2, 4, Inf),
+                              labels = c("Micro", "Unnoticeable", "Barely felt", "Uh Oh!"))), 
+                   alpha = 0.5)+ 
+  labs(
+    x = "Time",
+    y = "Earthquake Frequency",
+    fill = "Intensity",    # if you map fill = grouping var
+    title = "Vesuvius Earthquakes - How often and intense?",
+  )+ 
+  scale_fill_viridis_d(option = "magma")
